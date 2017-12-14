49f2d35d95b4946db9069989d57a2469dcadd5f7
derekbmcintire <derekbmcintire@gmail.com>
2017-12-14 16:49:52 -0500

    Create cart object in store

--- assets/scripts/store.js
+++ assets/scripts/store.js
@@ -1,6 +1,21 @@
 'use strict'
 
 const store = {
+  cart: {
+    products: [{
+      product_id: '5a32ce46687ac05005a0c45d',
+      quantity: 3
+    },
+    {
+      product_id: '5a32ce46687ac05005a0c45s',
+      quantity: 1
+    },
+    {
+      product_id: '5a32ce46687ac05005a0c45a',
+      quantity: 2
+    }],
+    total: 15
+  }
 }
 
 module.exports = store
