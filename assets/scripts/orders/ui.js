123d269af3c754786f8cda5b74ac42957ce49acf
derekbmcintire <derekbmcintire@gmail.com>
2017-12-15 09:43:26 -0500

    Save product data in store and display in cart
    - save product data in store.products
    - get product data from store and display in cart

--- assets/scripts/orders/ui.js
+++ assets/scripts/orders/ui.js
@@ -6,9 +6,9 @@ const cart = require('../templates/cart.handlebars')
 
 // const showCart = cart({ products: store.currentCart.currentProducts })
 
-const onShowCart = function (data) {
-  data = store.currentCart.currentProducts
-  console.log(data)
+const onShowCart = function () {
+  const data = store.products
+  console.log('store.products is ', data)
   console.log('this is onShowCart')
   $('.cart-products').append(cart({ products: data }))
 }
