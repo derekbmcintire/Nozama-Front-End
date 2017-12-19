884a9770bf806ce727fb655eddc68324ebf1a8a4
derekbmcintire <derekbmcintire@gmail.com>
2017-12-18 19:13:43 -0500

    Change url origin for stripe request
    - remove extra carrot in index.html
    
    Derek

--- assets/scripts/orders/events.js
+++ assets/scripts/orders/events.js
@@ -25,7 +25,7 @@ const checkout = function () {
       // Get the token ID to your server-side code for use.
       const sendToken = function (data) {
         return $.ajax({
-          url: 'https://still-thicket-16022.herokuapp.com',
+          url: 'https://still-thicket-16022.herokuapp.com/tokens',
           method: 'POST',
           data
         })
