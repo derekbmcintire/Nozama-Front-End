aae1833f0b2ec1522394861ffcf46183913bc894
derekbmcintire <derekbmcintire@gmail.com>
2017-12-19 14:42:52 -0500

    Clone app and change name to Mapp

--- assets/scripts/orders/events.js
+++ assets/scripts/orders/events.js
@@ -27,7 +27,7 @@ const checkout = function () {
       // Get the token ID to your server-side code for use.
       const sendToken = function (data) {
         return $.ajax({
-          url: 'https://still-thicket-16022.herokuapp.com/tokens',
+          url: 'https://dry-journey-16743.herokuapp.com/',
           method: 'POST',
           data
         })
